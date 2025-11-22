da56da3be58a0067701aa51a708483ded46c79e2
KHJ\rla45 <rla45789@naver.com>
2025-11-23 05:46:22 +0900

    숫자 2번 키 입력시 타워 설치 기능 구현 PreviewPrafab, TileType에 따른 색상 변경 구현

--- Assets/Scripts/Tower/TowerPlacer.cs
+++ Assets/Scripts/Tower/TowerPlacer.cs
@@ -7,57 +7,111 @@ public class TowerPlacer : MonoBehaviour
 {
     public Camera cam;
     public Tilemap tilemap;
-    public MapData mapData;
 
     public Grid grid;
+    public GameObject towerPrefab;
+    public GameObject towerPreviewPrefab;
+    private GameObject previewInstance;
+    private bool isPlacing = false;
 
-    private void Awake()
+    public MapData originalMapData;
+    private MapData mapData;
+
+    void Awake()
     {
+        mapData = Instantiate(originalMapData);
         if (grid == null && tilemap != null)
             grid = tilemap.layoutGrid;
     }
 
-    private void Update()
+    void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            TryPlaceTower();
+            StartPlacing();
+        }
+
+        if (isPlacing)
+        {
+            UpdatePreview();
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                CancelPlacing();
+            }
         }
     }
 
-    void TryPlaceTower()
+    void UpdatePreview()
     {
-        if (cam == null || tilemap == null || mapData == null || grid == null)
-        {
-            Debug.LogWarning("TowerPlacer 세팅이 안 됨");
-            return;
-        }
-
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-
         Plane plane = new Plane(grid.transform.up, grid.transform.position);
 
         if (plane.Raycast(ray, out float enter))
         {
             Vector3 worldPos = ray.GetPoint(enter);
-
             Vector3Int cellPos = grid.WorldToCell(worldPos);
+            Vector3 cellCenter = grid.GetCellCenterWorld(cellPos);
 
-            Debug.Log($"월드좌표: {worldPos}");
-            Debug.Log($"셀좌표(Grid기준): {cellPos}");
+            previewInstance.transform.position = cellCenter;
 
             CellType type = mapData.GetCellByTilemapCoord(cellPos.x, cellPos.y);
+            bool canPlace = type == CellType.Buildable;
 
-            Debug.Log($"타입: {type}");
-
-            if (type == CellType.Buildable)
-            {
-                Debug.Log("타워 설치 가능");
-            }
+            if (canPlace)
+                SetPreviewColor(Color.green);
             else
+                SetPreviewColor(Color.red);
+
+            if (canPlace && Input.GetMouseButtonDown(0))
             {
-                Debug.Log("타워 설치 불가");
+                PlaceTower(cellCenter, cellPos);
             }
         }
     }
+    void StartPlacing()
+    {
+        if (previewInstance != null)
+            Destroy(previewInstance);
+
+        previewInstance = Instantiate(towerPreviewPrefab);
+        previewInstance.SetActive(true);
+
+        SetPreviewColor(Color.red);
+        isPlacing = true;
+    }
+
+    void SetPreviewColor(Color color)
+    {
+        Color c = color;
+        c.a = 0.5f;
+
+        foreach (var rend in previewInstance.GetComponentsInChildren<SkinnedMeshRenderer>())
+        {
+            rend.material.color = c;
+        }
+    }
+    void PlaceTower(Vector3 pos, Vector3Int cellPos)
+    {
+        Instantiate(towerPrefab, pos, Quaternion.identity);
+
+        mapData.SetCell(cellPos.x - mapData.originX, cellPos.y - mapData.originY, CellType.Blocked);
+
+        if (previewInstance != null)
+            Destroy(previewInstance);
+
+        previewInstance = null;
+        isPlacing = false;
+    }
+
+    void CancelPlacing()
+    {
+        if (previewInstance != null)
+        {
+            Destroy(previewInstance);
+            previewInstance = null;
+        }
+        isPlacing = false;
+    }
+
 }
